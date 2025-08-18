d5139915a5632e037f964964292bd9da16b52fdf
Maulik-khambhdiya <maulikkhambhdiya444@gmail.com>
2025-08-18 17:41:49 +0530

    aggregate

--- controller/studentMark.js
+++ controller/studentMark.js
@@ -1,28 +1,68 @@
-let API=require("../model/studentMark")
+let API = require("../model/studentMark");
 
-
-
-exports.createData=async(req,res)=>{
-    try {
-        let data = req.body
-        const addData= await API.create(data)
-        res.status(201).json({
-            status:"success",
-            message:"data created successfully",
-            data:addData
-        })
-    } catch (error) {
-        res.status(404).json({
-            status:"error",
-            message:error.message
-            })
-
-    }
-}
+exports.createData = async (req, res) => {
+  try {
+    let data = req.body;
+    const addData = await API.create(data);
+    res.status(201).json({
+      status: "success",
+      message: "data created successfully",
+      data: addData,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
 
 exports.viewData = async (req, res) => {
   try {
-    const viewData = await API.find().populate("name");
+    //const viewData = await API.find().populate("name");
+
+    const viewData = await API.find().aggregate([
+      {
+        $lookup: {
+          from: "students",
+          localField: "name",
+          foreignField: "_id",
+          as: "StudentInfo",
+        },
+      },
+
+      {
+        $unwind: "$StudentInfo",
+      },
+      {
+        $addFields: {
+          Total: {
+            $sum: ["$s1", "$s2", "$s3"],
+          },
+          Min: {
+            $min: ["$s1", "$s2", "$s3"],
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "students",
+          localField: "name",
+          foreignField: "_id",
+          as: "StudentInformation",
+        },
+      },
+      {
+        $unwind: "$StudentInformation",
+      },{
+        $addFields : {
+          Max : {
+            $max: ['$s1', '$s2', '$s3']
+          }
+        }
+      }
+    ]); //pass as array
+
     res.status(200).json({
       status: "success",
       message: "data found successfully",
@@ -63,12 +103,14 @@ exports.editData = async (req, res) => {
   try {
     const editId = req.params.id;
 
-    const checkData = await API.findById(editId); 
+    const checkData = await API.findById(editId);
     // console.log(checkData);
 
     if (!checkData) throw new Error("Record not found");
 
-    const updateData = await API.findByIdAndUpdate(editId, req.body, {new: true});
+    const updateData = await API.findByIdAndUpdate(editId, req.body, {
+      new: true,
+    });
     res.status(200).json({
       status: "Success",
       message: "Data Update SuccessFully",
